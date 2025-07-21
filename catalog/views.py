8a70b4080adcb29841ae2c6a02a13bbb02b5e874
KirsanV <kirsanshalkhakov@yahoo.com>
2025-07-21 10:08:53 +0300

    homework№24

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
 
 
 def contacts(request):
@@ -19,3 +21,8 @@ def contacts(request):
         success_message = 'Ваше сообщение успешно отправлено! Спасибо.'
 
     return render(request, 'contacts.html', {'success_message': success_message})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
