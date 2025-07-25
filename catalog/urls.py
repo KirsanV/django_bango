3253e3283a31cb336699f171c052d27c30a4363e
KirsanV <kirsanshalkhakov@yahoo.com>
2025-07-25 11:21:31 +0300

    homework26.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,22 +1,6 @@
-# from django.urls import path
-# from catalog.apps import NewappConfig
-# from catalog.views import home, contacts
-# from django.conf import settings
-# from django.conf.urls.static import static
-#
-# app_name = NewappConfig.name
-#
-# urlpatterns = [
-#     path('', home, name='home'),
-#     path('contacts/', contacts, name='contacts'),
-# ]
-#
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-
 from django.urls import path
 from .views import HomeView, ContactsView, ProductDetailView
+from .views import ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = 'catalog'
 
@@ -24,4 +8,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/add/', ProductCreateView.as_view(), name='product_add'),
+    path('product/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
